b098da95757e3db769be5e6ba4ebb79d353348f7
ISHIMWE ESCHYLE BERSY <ishimweeschylebersy@gmail.com>
2021-06-12 19:58:09 +0200

    working on login

---
+++ resources/views/app.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>KGB Team | @yield('title')</title>
+</head>
+<body>
+    @yield('content')
+    <h1>HELLO it's berking</h1>
+</body>
+</html>
