953b58be45834fb81f400dcdc59709ef6217cbf3
ISHIMWE ESCHYLE BERSY <ishimweeschylebersy@gmail.com>
2021-06-15 12:49:34 +0200

    diplaying all the data on the home screen

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -4,15 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Note;
 
 class AppController extends Controller
 {
     
     public function home(Request $request) {
         $users = User::orderBy('created_at', 'desc')->get();
+        $notes = Note::orderBy('created_at', 'desc')->get();
         return view('ui.home',)
                 ->with('user', $request->user())
-                ->with('users', $users);
+                ->with('users', $users)
+                ->with('notes', $notes);
     }
 
     public function diary(Request $request) {
--- resources/views/ui/home.blade.php
+++ resources/views/ui/home.blade.php
@@ -8,8 +8,18 @@
 	@else
 		<h2 style="text-decoration: underline;">actual kgb members!</h2>
 		@foreach($users as $us)
-			{{ $us->username }}
+			<strong>{{ $us->username }}</strong>
+			<em><a href="{{ route('diary', $us->id) }}">his diary</a></em><br>
 		@endforeach
 	@endif
 	<!--all the notes paginated-->
+	@if(is_null($notes))
+		no notes, be the first one!
+	@else
+		<h2 style="text-decoration: underline;">recent notes!</h2>
+		@foreach($notes as $nt)
+			<h3>{{ $nt->title }}</h3>
+			<p>{{ $nt->body }}</p>
+		@endforeach
+	@endif
 @stop
